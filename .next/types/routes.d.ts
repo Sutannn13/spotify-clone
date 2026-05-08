d462b7049d04d91490457396dfe07d8eb04ff94a
Sutan Arlie Johan <sutanarliejohan@gmail.com>
2026-05-08 15:15:39 +0700

    fix: fix bug on play button and updat some feature

--- .next/types/routes.d.ts
+++ .next/types/routes.d.ts
@@ -53,10 +53,4 @@ declare global {
    * }
    * ```
    */
-  type LayoutProps<LayoutRoute extends LayoutRoutes> = {
-    params: Promise<ParamMap[LayoutRoute]>
-    children: React.ReactNode
-  } & {
-    [K in LayoutSlotMap[LayoutRoute]]: React.ReactNode
-  }
-}
+  type Lay
